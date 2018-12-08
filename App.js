55790fe54fe46b2bd400376df10f45db30db67dd
Arief Yusron <ariefyusron98@gmail.com>
2018-12-08 19:21:24 +0700

    logika

--- App.js
+++ App.js
@@ -1,40 +1,65 @@
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+import { FlatList,TouchableOpacity } from 'react-native';
+import { Container, Text, Input, Row, Col, Content, Button, Item, List, ListItem } from 'native-base';
 
 export default class App extends Component {
+
+  constructor(){
+    super();
+    this.state={
+      set: '',
+      data: []
+    };
+  }
+
+  add = () => {
+    set = this.state.set;
+    if(set.length>0){
+      data = this.state.data.concat([set]);
+      this.setState({data:data});
+      this.setState({set:''})
+    } else{
+      alert('Please input your to do.')
+    }
+  }
+
+  set(text){
+    this.setState({set:text});
+  }
+
+  delete(i){
+    data = this.state.data.slice();
+    data.splice(i,1);
+    this.setState({data:data});
+  }
+
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
-      </View>
+      <Container>
+        <Content>
+          <Row style={{justifyContent:'center'}}>
+            <Col style={{flex:0.9}}>
+              <Item style={{borderBottomColor:'white'}}>
+                <Input value={this.state.set} onChangeText={(text)=>this.set(text)} placeholder='Type to add to do List' style={{backgroundColor:'red',}} />
+                <Button onPress={this.add} style={{height:'100%'}}>
+                  <Text>Add</Text>
+                </Button>
+              </Item>
+            </Col>
+          </Row>
+          <FlatList
+            data={this.state.data}
+            renderItem={
+              ({item, index}) =>
+                <ListItem style={{paddingBottom:0,paddingTop:0,height:50}}>
+                  <TouchableOpacity style={{flex:1,height:100+'%',justifyContent:'center'}} onLongPress={()=>this.delete(index)}>
+                    <Text style={{backgroundColor:'red',width:100+'%'}}>{item}</Text>
+                  </TouchableOpacity>
+                </ListItem>
+            }
+          />
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
